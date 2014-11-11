d2a7818af609e9884f199f68f44556ed854a35b4
Anton Boytsov <antonboytsov@gmail.com>
2014-11-12 01:41:26 +0600

    Refactor + adding chapter draft

--- www/text/chapter.php
+++ www/text/chapter.php
@@ -1,44 +1,100 @@
 <?
-  //=================================== Обработка параметров
-  $text_id = intval($_GET["text_id"]);
-  $chapter_id = intval($_GET["chapter_id"]);
-  if ($text_id <= 0 || $chapter_id <= 0) {
-    header('Location: /');
-    die();
-  }
 
-  //=================================== Авторизация
+	#==== Load page ===========================
+	
+	$text_id = intval($_GET["text_id"]);
+	$chapter_id = intval($_GET["chapter_id"]);
+	if ($text_id <= 0 || $chapter_id <= 0)
+	{
+		header('Location: /');
+		die();
+    }
+    
+    #==== Load page ===========================
+    
+    
+    #==== Load database =======================
 
-  include('../mod_db.php');
-  include('../mod_auth.php');
-  include('../code/text_format.php');
+	include('../mod_db.php');
+	include('../mod_auth.php');
+	include('../code/text_format.php');
+
+	mysql_query("SET NAMES utf8");
+
+	$text_query = 'SELECT `text`.*, `user`.`name` AS `creator_name` FROM `text` JOIN `user` ON(`creator` = `user_id`) WHERE `text_id` = ' . $text_id;
+	$fragment_query = 'SELECT `fragment_id`, `text` FROM `original_fragments` WHERE `text_id` = ' . $text_id;
+
+	$text_result = mysql_query($text_query);
+	$fragment_result = mysql_query($fragment_query);
+
+	$text_row = mysql_fetch_assoc($text_result);
+
+	if (!$text_row) {
+		header('Location: /error/text_not_found.php');
+		die();
+	}
+
+	$user -> loadTextOut($text_row);
+
+	if ($text_row["is_deleted"])
+	{
+		header('Location: /error/text_not_found.php');
+		die();
+	}
+	
+	#==== Load database =======================
+	
+	function like()
+{
+	echo "Like!";
+}
+
+function gen_chapter_table_content($text_result, $fragment_result)
+{
+	
+	$i = 0;
+	while ($fragment_row = mysql_fetch_assoc($fragment_result))
+	{
+		
+		$trans_query = 'SELECT `translation_id`, `fragment_id`, `text` FROM `translations` WHERE 
+			`fragment_id` = ' . $fragment_row["fragment_id"];
+		$trans_result = mysql_query($trans_query);
+		
+		$rows_count = mysql_num_rows($trans_result) + 1;
+		$first_row = mysql_fetch_assoc($trans_result);
+
+		$i++;
+		print "<tr><td rowspan=\"" . $rows_count . "\">" . $i . "</td>
+			<td rowspan=\"" . $rows_count . "\">" . $fragment_row["text"] . "</td>";
+		
+		if ($first_row)
+		{
+			print "<td>" . $first_row["text"] . "</td>
+				<td><input type=\"button\" value=\"Like\"></td>
+				<td><input type=\"button\" value=\"Disike\"></td>
+				</tr>";
+			while ($trans_row = mysql_fetch_assoc($trans_result))
+			{
+				print "<tr><td>" . $trans_row["text"] . "</td>
+					<td><input type=\"button\" value=\"Like\"></td>
+					<td><input type=\"button\" value=\"Disike\"></td>
+					</tr>";
+			}
+			print "<tr><td colspan=3><input type=\"button\" value=\"Add...\"></td></tr>";
+		}
+		else
+		{
+			print "<td colspan=3>Пока переводов нет. <input type=\"button\" value=\"Add...\"></td></tr>";
+		}
+						
+		print "\n";
+	}
+	
+}
   
-  mysql_query("SET NAMES utf8");
-
-  $query = 'SELECT `text`.*, `user`.`name` AS `creator_name` FROM `text` JOIN `user` ON(`creator` = `user_id`) WHERE `text_id` = ' . $text_id;
-  $fragment_query = 'SELECT `fragment_id`, `text` FROM `original_fragments` WHERE `text_id` = ' . $text_id;
-  
-  $result = mysql_query($query);
-  $fragment_result = mysql_query($fragment_query);
-  
-  $text_row = mysql_fetch_assoc($result);
-  
-  if (!$text_row) {
-    header('Location: /error/text_not_found.php');
-    die();
-  }
-
-  $user -> loadTextOut($text_row);
-
-  if ($text_row["is_deleted"]) {
-    header('Location: /error/text_not_found.php');
-    die();
-  }
-  function additionalPageHeader() {
 ?>
   <link rel="stylesheet" type="text/css" href="/styles/chapter_main.css">
-<?}
-
+<?
   $title = 'Редактирование';
   include('../header.php');
 ?>
@@ -58,44 +114,7 @@
 			</tr>
 		
 		<?
-			$i = 0;
-			while ( $fragment_row = mysql_fetch_assoc($fragment_result))
-			{
-				
-				$trans_query = 'SELECT `translation_id`, `fragment_id`, `text` FROM `translations` WHERE 
-					`fragment_id` = ' . $fragment_row["fragment_id"];
-				$trans_result = mysql_query($trans_query);
-				
-				$rows_count = mysql_num_rows($trans_result);
-				$first_row = mysql_fetch_assoc($trans_result);
-				
-				if (!$rows_count)
-					$rows_count = 1;
-
-				$i++;
-				print "<tr><td rowspan=\"" . $rows_count . "\">" . $i . "</td>
-					<td rowspan=\"" . $rows_count . "\">" . $fragment_row["text"] . "</td>";
-				
-				if ($first_row)
-				{
-					print "<td>" . $first_row["text"] . "</td>
-						<td><input type=\"button\" value=\"Like\"></td>
-						<td><input type=\"button\" value=\"Disike\"></td>
-						</tr>";
-				}
-				else
-				{
-					print "<td colspan=3>Пока переводов нет.</td></tr>";
-				}
-				while ($trans_row = mysql_fetch_assoc($trans_result))
-				{
-					print "<tr><td>" . $trans_row["text"] . "</td>
-						<td><input type=\"button\" value=\"Like\"></td>
-						<td><input type=\"button\" value=\"Disike\"></td>
-						</tr>";
-				}
-				print "\n";
-		    }
+			gen_chapter_table_content($text_result, $fragment_result);
 	    ?>
 		
 		</table>
