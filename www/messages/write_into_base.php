2eb4d294cfd7ef50dc385c772866104c8e395270
RustamRimovich <valimuhammatov@mail.ru>
2014-11-20 20:00:46 +0300

    Добавлено написание письма

--- www/messages/write_into_base.php
+++ www/messages/write_into_base.php
@@ -5,14 +5,24 @@
 	
 	$id=$user -> uid;
 	$text=$_POST["answer_area"];
-	$id_user_from=$_POST["id_user_from"];
-	
-	//$id_user_from=5;
-	
+	$id_user_to=$_POST["id_user_from"];
+
+	if (empty ($id_user_to)) 
+	{	
+		$name = $_POST["recipient"];
+
+		$sqlQueryUserId = "SELECT `user_id` FROM `user` WHERE `name` = '$name'";
+		$rsUserId= mysql_query($sqlQueryUserId);
+		if (mysql_num_rows($rsUserId) > 0)
+		{
+			$idrow = mysql_fetch_row($rsUserId);
+			$id_user_to = $idrow[0];
+		}
+		else $id_user_to = 1;
+	}
 	if($text<>null) {
-		$sqlQueryInsert = "INSERT INTO `message` VALUES (NULL,'$id','$id_user_from','$text',NULL,0,0,0)";
+		$sqlQueryInsert = "INSERT INTO `message` VALUES (NULL,'$id','$id_user_to','$text',NULL,0,0,0)";
 		$rs = mysql_query($sqlQueryInsert);
-		//echo("Mail successed sented!");
 	}
 	
 	include('../header.php');
