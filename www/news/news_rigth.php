017863b2ca5743d7f74af713f1b0611cace12d0a
MariaFL <fedmaria2@gmail.com>
2014-12-17 16:07:54 +0500

    Файлик для вывода новостей справа

--- www/news/news_rigth.php
+++ www/news/news_rigth.php
@@ -1,11 +1,12 @@
 <?
-			$result = mysql_query("SELECT * FROM news ORDER BY news_id DESC LIMIT 5") or die(mysql_error());
-			if (mysql_num_rows($result)>0){  
-				while ($data = mysql_fetch_assoc($result)){  
-					printf('<div class="news_title">
+	$result = mysql_query("SELECT * FROM news ORDER BY news_id DESC LIMIT 5") or die(mysql_error());
+	if (mysql_num_rows($result)>0){  
+		while ($data = mysql_fetch_assoc($result)){  
+			printf('<div class="news_title">
 						<a href="view.php?id=%s"><p> %s </p></a>
 						<p>%s</p>
 					</div>',$data["news_id"], $data["title"], $data["date"]);
-				}}
+				}
+	}
 			
-		?>
+?>
