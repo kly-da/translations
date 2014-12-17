851bfc8e1dba7291e36748c6e92f79536780a3e8
MariaFL <fedmaria2@gmail.com>
2014-12-17 10:20:30 +0500

    Стили лист новостей

--- www/news/list.php
+++ www/news/list.php
@@ -1,1 +1,42 @@
-﻿<?php echo 'Привет от PHP'; ?>
+﻿<?
+  include('../mod_db.php');
+  include('../mod_auth.php');
+
+  $title = "News";
+  include('../header.php');
+?>
+
+  <head>
+	<link rel="stylesheet" tipe="text/css" href="css/style.css" />
+  </head>
+  <body>
+  
+	<div class="content" style="border: 0px;">
+		
+		<?
+			$result = mysql_query("SELECT * FROM news") or die(mysql_error());
+			$data = mysql_fetch_array($result);
+			
+			do {
+				printf('<div class="article">
+					<?
+						include("content.php");
+					?>
+				</div>',$data["title"], $data["news_text"]);
+			}
+			while($data = mysql_fetch_array($result));
+			
+		?>
+		
+	</div>
+	
+	<div class="user">
+		<div class="middle_text">Пользователь</div>
+	</div>
+	<div style="clear:right;"/></div>
+	<div class="news">
+    <div class="middle_text">Новости</div>
+	</div>
+  </body>
+
+<? include('../footer.php');?> 
